650b62a753f17053bad87278eedcddacdd135a65
chiki <work@chiki.dev>
2023-09-13 19:05:08 +0300

    chezmoi is confusing

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -19,12 +19,12 @@ set -Ux GOPATH $HOME/.go
 zoxide init fish | source
 theme_gruvbox dark
 alias cat=bat
-alias ls='exa'                                                          # ls
-alias l='exa -lbF --git'                                                # list, size, type, git
-alias ll='exa -lbGF --git'                                             # long list
-alias llm='exa -lbGd --git --sort=modified'                            # long list, modified date sort
-alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
-alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
+alias ls='exa --icons'                                                          # ls
+alias l='exa --icons --icons-lbF --git'                                                # list, size, type, git
+alias ll='exa --icons -lbGF --git'                                             # long list
+alias llm='exa --icons -lbGd --git --sort=modified'                            # long list, modified date sort
+alias la='exa --icons -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
+alias lx='exa --icons -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
 alias sw='/home/chiki/.config/sway/scripts/sway'
 set -g fzf_fd_opts --hidden --exclude=.git --exclude=go --exclude=.rustup --exclude=.cargo --exclude=.cache --exclude=.mozilla --exclude=.npm --exclude=.local
 clear
