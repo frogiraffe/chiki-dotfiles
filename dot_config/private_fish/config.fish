74d989c2410f42e82f2d6fab1fbad5623bbff006
chiki <work@chiki.dev>
2023-09-17 21:59:48 +0300

    added yazi instead of ranger

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -7,7 +7,6 @@ if test -z $DISPLAY; and test (tty) = "/dev/tty1"
     ~/.config/sway/scripts/sway
 end
 
-
 # Created by `pipx` on 2023-08-25 19:33:11
 set PATH $PATH /home/chiki/.local/bin
 # sudoedit
@@ -19,12 +18,12 @@ set -Ux GOPATH $HOME/.go
 zoxide init fish | source
 theme_gruvbox dark
 alias cat=bat
-alias ls='exa --icons'                                                          # ls
-alias l='exa --icons --icons-lbF --git'                                                # list, size, type, git
-alias ll='exa --icons -lbGF --git'                                             # long list
-alias llm='exa --icons -lbGd --git --sort=modified'                            # long list, modified date sort
-alias la='exa --icons -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
-alias lx='exa --icons -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
+alias ls='eza --icons'                                                          # ls
+alias l='eza --icons --icons-lbF --git'                                                # list, size, type, git
+alias ll='eza --icons -lbGF --git'                                             # long list
+alias llm='eza --icons -lbGd --git --sort=modified'                            # long list, modified date sort
+alias la='eza --icons -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
+alias lx='eza --icons -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
 alias sw='/home/chiki/.config/sway/scripts/sway'
 alias hertz='sh /home/chiki/.config/sway/scripts/hertz.sh'
 set -g fzf_fd_opts --hidden --exclude=.git --exclude=go --exclude=.rustup --exclude=.cargo --exclude=.cache --exclude=.mozilla --exclude=.npm --exclude=.local
